abf979714101109ceb3978a727fc4052990d87b3
unknown <nwadiaromiraclechukwuma@gmail.com>
2022-11-06 04:25:58 +0000

    changes made

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://Nwadiaro:Exceptional123@nwadiaromiracle.knajwok.mongodb.net/Api?retryWrites=true&w=majority'
 const connectDB = (url) => { return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
